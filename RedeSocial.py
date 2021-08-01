6d3f0ad4eec4b99e3246c022d76e49681dd13152
Letícia Vitória <leticiav_gomes@hotmail.com>
2021-08-01 11:16:52 -0300

    Initial commit

---
+++ RedeSocial.py
@@ -0,0 +1,54 @@
+
+pessoas = ["Alice", "Bob", "Carol", "Danielle"]
+for i,pessoas in enumerate(pessoas):
+    print(str(i)+":" + pessoas)
+
+
+amizades = [[0,0,0,1],[1,0,1,1],[0,0,0,1],[1,1,0,0]]
+for i in range(0, 4):
+    for j in range(0, 4):
+        amizades[i][j]
+        print(f'[{amizades[i][j]}]', end= ' ')
+    print()
+   
+
+def exibe_amigos(pessoas, amizades, nomePessoa):
+    for i in range(len(pessoas)):
+        if pessoas[i] in nomePessoa:
+            print(nomePessoa)
+        for j in range(0, 4):
+                if amizades[i][j] == 1:
+                    print("Lista amigos da " + nomePessoa + ": ")
+                    return print(pessoas[j])
+        else:
+                print("Não tem amigos!")
+    else: 
+        return print("Nome não encontrado!")
+
+
+def exibe_amigos_comum(pessoas, nomePessoaX, nomePessoaY):
+    tam = len(pessoas)
+    for i in range(tam):
+        if pessoas[i] in nomePessoaX:
+            for j in range(tam): 
+                if pessoas[j] in nomePessoaY:
+                    for m in range(0, 4):
+                        if amizades[i][m] == 1 and amizades [j][m] == 1:
+                            return print("Amigos em comum entre " + nomePessoaX + " e " + nomePessoaY +
+                        ": " + pessoas[m])
+            else: 
+                print("Não tem amigos em comum.")
+    else: 
+        return print("---")
+
+# chama a função exibe_amigos
+tentativa = exibe_amigos(pessoas = ["Alice", "Bob", "Carol", "Danielle"], amizades = [[0,0,0,1],[1,0,1,1],[0,0,0,1],[1,1,0,0]], nomePessoa="Bob")
+
+#chama a função exibe_amigos_comum
+tent = exibe_amigos_comum(pessoas=["Alice", "Bob", "Carol", "Danielle"], nomePessoaX="Alice", nomePessoaY="Danielle")
+
+
+
+
+    
+
