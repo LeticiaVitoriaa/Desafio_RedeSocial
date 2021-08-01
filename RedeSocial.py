3686b276edd31fe1b9dd5801e381ec43f902c847
Letícia Vitória <leticiav_gomes@hotmail.com>
2021-08-01 12:13:56 -0300

    Update-01/08

--- RedeSocial.py
+++ RedeSocial.py
@@ -3,6 +3,7 @@ pessoas = ["Alice", "Bob", "Carol", "Danielle"]
 for i,pessoas in enumerate(pessoas):
     print(str(i)+":" + pessoas)
 
+print("----------------------")
 
 amizades = [[0,0,0,1],[1,0,1,1],[0,0,0,1],[1,1,0,0]]
 for i in range(0, 4):
@@ -10,7 +11,8 @@ for i in range(0, 4):
         amizades[i][j]
         print(f'[{amizades[i][j]}]', end= ' ')
     print()
-   
+
+print("----------------------")
 
 def exibe_amigos(pessoas, amizades, nomePessoa):
     for i in range(len(pessoas)):
@@ -18,7 +20,7 @@ def exibe_amigos(pessoas, amizades, nomePessoa):
             print(nomePessoa)
         for j in range(0, 4):
                 if amizades[i][j] == 1:
-                    print("Lista amigos da " + nomePessoa + ": ")
+                    print("Lista amigos de " + nomePessoa + ": ")
                     return print(pessoas[j])
         else:
                 print("Não tem amigos!")
@@ -28,24 +30,28 @@ def exibe_amigos(pessoas, amizades, nomePessoa):
 
 def exibe_amigos_comum(pessoas, nomePessoaX, nomePessoaY):
     tam = len(pessoas)
+    
     for i in range(tam):
         if pessoas[i] in nomePessoaX:
             for j in range(tam): 
                 if pessoas[j] in nomePessoaY:
                     for m in range(0, 4):
                         if amizades[i][m] == 1 and amizades [j][m] == 1:
-                            return print("Amigos em comum entre " + nomePessoaX + " e " + nomePessoaY +
-                        ": " + pessoas[m])
+                            print("Amigos em comum entre " + nomePessoaX + " e " + nomePessoaY +
+                        ": " )
+                        return print(pessoas[m])
             else: 
                 print("Não tem amigos em comum.")
     else: 
-        return print("---")
+        return print(".")
 
 # chama a função exibe_amigos
 tentativa = exibe_amigos(pessoas = ["Alice", "Bob", "Carol", "Danielle"], amizades = [[0,0,0,1],[1,0,1,1],[0,0,0,1],[1,1,0,0]], nomePessoa="Bob")
 
+print("----------------------")
+
 #chama a função exibe_amigos_comum
-tent = exibe_amigos_comum(pessoas=["Alice", "Bob", "Carol", "Danielle"], nomePessoaX="Alice", nomePessoaY="Danielle")
+tent = exibe_amigos_comum(pessoas=["Alice", "Bob", "Carol", "Danielle"], nomePessoaX="Bob", nomePessoaY="Danielle")
 
 
 
